5524d3fee3f06d9e17774ae9e492f42f081a0745
Kata Wohlmuth <katawohl@gmail.com>
2020-03-26 16:21:27 +0100

    adde extra condition to LineRemover

--- src/todo/LineRemover.java
+++ src/todo/LineRemover.java
@@ -17,16 +17,19 @@ public class LineRemover {
 
   public void removeLine(String[] args) throws IOException {
     int lineToRemove = Integer.parseInt(args[1]) - 1;
-    List<String> allLines = Files.readAllLines(Paths.get(path));
-    List<String> linesRemoved = new ArrayList<>();
-    for (int i = 0; i < allLines.size(); i++) {
-      if (i == lineToRemove) {
-        continue;
+    if (args.length >= lineToRemove) {
+      List<String> allLines = Files.readAllLines(Paths.get(path));
+      List<String> linesRemoved = new ArrayList<>();
+      for (int i = 0; i < allLines.size(); i++) {
+        if (i == lineToRemove) {
+          continue;
+        }
+        linesRemoved.add(allLines.get(i));
       }
-      linesRemoved.add(allLines.get(i));
+      Files.write(Paths.get(path), linesRemoved);
+    } else {
+      System.out.println("There aren't as many todos, phew!");
     }
-    Files.write(Paths.get(path), linesRemoved);
     lister.listTask(path);
-
   }
 }
